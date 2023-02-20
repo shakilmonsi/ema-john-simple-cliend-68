8cbb6ef85ead2ad407295b5d3d3bef082bf0c289
unknown <samdshakilofficial@gmail.com>
2023-02-20 18:12:10 +0500

    add number button and page state

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import {
+  addToDb,
+  deleteShoppingCart,
+  getStoredCart,
+} from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
+
+const Shop = () => {
+  const { products, count } = useLoaderData();
+  // const products = useLoaderData();
+  const [cart, setCart] = useState([]);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const pages = Math.ceil(count / size);
+
+  const clearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
+
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product._id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart);
+  }, [products]);
+
+  const handleAddToCart = (selectedProduct) => {
+    console.log(selectedProduct);
+    let newCart = [];
+    const exists = cart.find((product) => product._id === selectedProduct._id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter(
+        (product) => product._id !== selectedProduct._id
+      );
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
+
+    setCart(newCart);
+    addToDb(selectedProduct._id);
+  };
+
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            key={product._id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart clearCart={clearCart} cart={cart}>
+          <Link to="/orders">
+            <button>Review Order</button>
+          </Link>
+        </Cart>
+      </div>
+      <div className="pagination">
+        <p>Currently Selected page{page}</p>
+        {[...Array(pages).keys()].map((number) => (
+          <button
+            key={number}
+            className={page === number && "selected"}
+            onClick={() => setPage(number)}
+          >
+            {number}{" "}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsKcFA1y1gCTeVQzs2I-fVuwKa0yq3mXc",
+  authDomain: "care-service-a5d19.firebaseapp.com",
+  projectId: "care-service-a5d19",
+  storageBucket: "care-service-a5d19.appspot.com",
+  messagingSenderId: "880044295840",
+  appId: "1:880044295840:web:b60843b66da904015b2bc5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
